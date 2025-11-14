8a7a4a58b01e64990fae1f0e70f1c5fe8eb5b180
cornelius <cornelius.lejeune@googlemail.com>
2025-11-14 19:57:55 +0100

    Added: Visualization of cell status (analyzed, infeced, contained) on board

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -1,13 +1,20 @@
 const Cell = (props) => {
-  const cellStyle = {
+  const baseStyle = {
     aspectRatio: '1',
     border: '1px solid cyan',
     background: 'darkcyan',
   }
 
+  const getCellStyle = () => {
+    if (props.isInfected) return { ...baseStyle, background: 'red', borderColor: 'red' }
+    if (props.isContained) return { ...baseStyle, background: 'cyan' }
+    if (props.isAnalyzed) return { ...baseStyle, background: 'black', borderColor: 'black' }
+    return { ...baseStyle, background: 'darkcyan' } 
+  }
+
   return (
     <div
-      style={cellStyle}
+      style={getCellStyle()}
       onClick={props.onClick}
       onContextMenu={props.onContextMenu}
     />
